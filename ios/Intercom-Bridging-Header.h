e19b654d76fe06cb3bdff638c59e1488d73c9c3d
Jakub Hanko <1kuko3@gmail.com>
2025-05-12 14:39:35 +0200

    attempt with just bridging header file

---
+++ ios/Intercom-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Intercom-Bridging-Header.h
+//  Intercom
+//
+//  Created by Jakub Hanko on 12.05.2025.
+//  Copyright © 2025 Facebook. All rights reserved.
+//
+
+#ifndef Intercom_Bridging_Header_h
+#define Intercom_Bridging_Header_h
+#import <IntercomModule.h>
+
+#endif /* Intercom_Bridging_Header_h */
